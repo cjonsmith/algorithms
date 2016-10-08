c611f964ca4b8e0ebff8b91bd49c506df7bc8fea
Cameron Smith <cameronsmith@email.arizona.edu>
2016-10-08 16:00:30 -0700

    updated insertion sort algorithm

--- sorting_algorithms/insertion_sort.py
+++ sorting_algorithms/insertion_sort.py
@@ -1,8 +1,24 @@
 def insertion_sort(a_list):
+    """Sorts a given list using the insertion sort algorithm.
+
+    :param a_list: The list to be sorted
+    :return: The sorted list
+    """
+    # Start at the second element since the first element is considered sorted.
     for i in range(1, len(a_list)):
-        if a_list[i] < a_list[i - 1]:
-            for j in range(i, -1):
-                if a_list[i] < a_list[j]:
-                    a_list[i], a_list[j] = a_list[j], a_list[i]
-                    i-=1
+        
+        # Check if the current element is ever greater than any of the previous
+        # elements. If it is, continue to swap it backwards into the list.
+        for j in range(i, 0, -1):
+            if a_list[j] < a_list[j - 1]:
+                tmp = a_list[j - 1]
+                a_list[j - 1] = a_list[j]
+                a_list[j] = tmp
+            # If the current element is greater than the previous element, then
+            # we know the list is in the correct order, since the inner loop only
+            # operates on the sorted portion of the list.
+            else:
+                break
+    
+    return a_list
 
